ecc7dc52c38d07fdafdb2714f723217f52f5bc52
Prashant6991 <50472119+Prashant6991@users.noreply.github.com>
2024-08-28 22:49:02 +0530

    Add files via upload

--- trading_framework/execution_client.py
+++ trading_framework/execution_client.py
@@ -1,26 +1,46 @@
-from typing import Protocol
-
+#from typing import Protocol
+from typing_extensions import Protocol
 
 class ExecutionException(Exception):
-    pass
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
 
 
 class ExecutionClient(Protocol):
 
-    def buy(self, product_id: str, amount: int):
+    def buy(product_id: str, amount: int, price:float):
         """
         Execute a buy order, throws ExecutionException on failure
         :param product_id: the product to buy
         :param amount: the amount to buy
         :return: None
         """
-        ...
+        try:
+            buy_order = {
+                "action": "buy",
+                "product_id": product_id,
+                "amount": amount,
+                "price": price
+            }
+            print("Buy order placed for " + str(amount) + " shares of " + str(product_id) + " at " + str(price))
+        except Exception as e:
+            raise ExecutionException("Error Buying Order------" + str(e))
 
-    def sell(self, product_id: str, amount: int):
+    def sell(product_id: str, amount: int, price:float):
         """
         Execute a sell order, throws ExecutionException on failure
         :param product_id: the product to sell
         :param amount: the amount to sell
         :return: None
         """
-        ...
+        try:
+            sell_order = {
+                "action": "sell",
+                "product_id": product_id,
+                "amount": amount,
+                "price": price
+            }
+            print("Sell order placed for " + str(amount) + " shares of " + str(product_id) + " at " + str(price))
+        except Exception as e:
+            raise ExecutionException("Error Selling Order------" + str(e))
--- trading_framework/price_listener.py
+++ trading_framework/price_listener.py
@@ -1,5 +1,5 @@
-from typing import Protocol
-
+# from typing import Protocol
+from typing_extensions import Protocol
 
 class PriceListener(Protocol):
 
